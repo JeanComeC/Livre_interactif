5a0a529c6e6f7ccb5ec66705bd3ac360ef50c41f
Alexandreblot <alexandre.blot@isen-ouest.yncrea.fr>
2025-06-12 10:38:11 +0200

    ptit

--- src/import.c
+++ src/import.c
@@ -1,1 +1,25 @@
 /*fichier pour mettre toute les fonctions pour récupérer les chapitres*/
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+#define LINE_SIZE 512
+
+void trouverchap(){
+FILE* file = fopen(book.txt, "r");
+if (file == NULL) {
+  printf("Unable to open file <%s>\n", book.txt);
+  exit(EXIT_FAILURE);
+}
+
+char line[LINE_SIZE];
+while(fgets(line, sizeof(line), file)) {
+    if(){
+        
+    }
+}
+}
