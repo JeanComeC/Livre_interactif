3ecf7c691bdfd3b68b718501fd0d719a9236f033
Alexandre Blot <alexandre.blot@isen-ouest.yncrea.fr>
2025-06-12 11:07:47 +0200

    ptite modif

--- src/import.c
+++ src/import.c
@@ -10,16 +10,23 @@
 #define LINE_SIZE 512
 
 void trouverchap(){
-FILE* file = fopen(book.txt, "r");
+FILE* file = fopen("src/book.txt", "r");
 if (file == NULL) {
-  printf("Unable to open file <%s>\n", book.txt);
+  printf("Unable to open file <%s>\n", "src/book.txt");
   exit(EXIT_FAILURE);
 }
 
 char line[LINE_SIZE];
 while(fgets(line, sizeof(line), file)) {
-    if(){
-        
+    if(strstr(line, "<chapter id") != NULL){
+        printf("Found chapter: %s", line);
+        int id = 0;
+        char* titre;
+        sscanf(line,"<chapter id=\"%d\">%[^<]s", &id, &titre);
+        printf("Found id and title: %d %s", id, titre);
     }
 }
+}
+int main(){
+  trouverchap();
 }
