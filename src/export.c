5f8237667acf065b16f68409b56ee353cd3b8496
Paul-hr1 <paul.huray@isen-ouest.yncrea.fr>
2025-06-13 10:32:46 +0200

    commentaires

--- src/export.c
+++ src/export.c
@@ -2,25 +2,25 @@
 
 #include "export.h"
 
-void creerHTML(char* nom){
-	fopen(nom, "w");
+void creerHTML(char* nom){ // fonction pour créer un fichier html
+	fopen(nom, "w"); 
 }
 
-void ecrireIdTitre(char* nom,int id, char* titre){
+void ecrireIdTitre(char* nom,int id, char* titre){ // fonction pour écrire le numéro et le titre du chaapitre dans le fichier html
 	FILE* f = fopen(nom,"w");
 	fprintf(f,"<h1>Chapitre %d : %s</h1>",id,titre);
 }
 
-void ecrireParagraphe(char* nom,char* paragraphe){
-	FILE* f = fopen(nom,"w");
+void ecrireParagraphe(char* nom,char* paragraphe){ // fonction pour écrire les paragraphes dans le fichier html
+	FILE* f = fopen(nom,"w"); 
 	fprintf(f,"<p>%s</p>",paragraphe);
 }
 
-void ecrireChoix(char* nom,int id, char* line){
+void ecrireChoix(char* nom,int id, char* line){ // fonction pour écrire le choix dans le fichier html
 	FILE* f = fopen(nom,"w");
 	char contenu[LINE_SIZE];
 	char texteLien[LINE_SIZE];
-	sscanf(line,"<choice idref= [^>]%[^<]s",&contenu);
+	sscanf(line,"<choice idref= [^>]%[^<]s",&contenu); 
 	sscanf(line,"<a [^>]%[^<]s",texteLien);
 	fprintf(f, "<p>%s</p><a href=\"%d.html\">%s</a>",contenu,id,texteLien);
 }
