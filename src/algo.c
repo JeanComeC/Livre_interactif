b8a1166a31e7ab4a1fcfcca7346c8a6316469cfb
Paul-hr1 <paulhuray76@gmail.com>
2025-06-16 10:19:29 +0200

    c'est le bordel
    fonctions struct Item/Inventaire

--- src/algo.c
+++ src/algo.c
@@ -1,3 +1,49 @@
 //fichier pour mettre les fonctions pour toute l'algorythmique du jeu
 
 #include "algo.h"
+
+
+struct Inventaire init_tab_inventaire(){
+    struct Inventaire inventaire;
+    struct Item* init = malloc(3*sizeof(struct Item));
+    if(init == NULL){
+        fprintf(stderr,"Memory error");
+        exit(1);
+    }
+    inventaire.item = init;
+    inventaire.size = 0;
+    inventaire.capacity = 3;
+    return inventaire;
+}
+
+void add_tab_inventaire(struct Inventaire* tab, struct Item new_item){
+    if(tab->size >= tab->capacity){
+        return;
+    }
+    tab -> item[tab->size++] = new_item;
+}
+
+void remove_item_tab_inventaire(struct Inventaire* tab, char* rem_item){
+    for(int i=0; i<tab->size;i++){
+        if(strcmp(tab->item[i].name,rem_item) == 0){
+            tab->item[i] = tab->item[tab->size];
+            tab->size --;
+        }
+    }
+}
+
+void destroy_tab_inventaire(struct Inventaire* inventaire){
+    free(inventaire->item);
+    inventaire->item = NULL;
+    inventaire->capacity=0;
+    inventaire->size=0;
+}
+
+bool get_tab_inventaire(struct Inventaire* tab, char* item){
+    for(int i=0;i<tab->size;i++){
+        if(strcmp(tab[i].item->name,item) == 0){
+            return true;
+        }
+    }
+    return false;
+}
--- src/algo.h
+++ src/algo.h
@@ -4,16 +4,61 @@
 //Structures
 struct Item{
     char* name;
-    int size;
     int pv;
     int degat;
 };
+
+struct Inventaire{
+    int size;
+    int capacity;
+    struct Item* item;
+};
+
+struct StringArray{
+    char** text;
+    int size;
+    int capacity;
+};
+
+struct Choice{
+    char text[512];
+    int nextChapter;
+    char item[256];
+};
+
+struct ChoicesArray{
+    struct Choice choice;
+};
+
+struct Chapter{
+    char title[50];
+    struct StringArray paragraphe;
+    struct ChoicesArray choices;
+    struct Item item;
+    struct StringArray options;
+};
+
+struct BigTableau{
+    struct Chapter* chapter;
+    int size;
+    int capacity;
+};
+
 //MACRO
 
 //BIBLIOTHEQUE
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
 //PROTOTYPES
 
+struct Inventaire init_tab_inventaire();
+void add_tab_inventaire(struct Inventaire* tab, struct Item new_item); 
+void remove_item_tab_inventaire(struct Inventaire* tab, char* rem_item);
+void destroy_tab_inventaire(struct Inventaire* inventaire);
+bool get_tab_inventaire(struct Inventaire* tab, char* item);
 
 #endif
