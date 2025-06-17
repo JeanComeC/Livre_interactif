d0ac153a1425e99e598322356469e6dd5365e255
JeanComeC <jean-come.chopin@isen-ouest.yncrea.fr>
2025-06-17 13:10:44 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -19,13 +19,13 @@ int main(){
     
     
 // Affichage :
-    int* id_prochain_chapitre = 0;
-    struct Chapter* chapitre_actuel = &BigTableau.chapter[0];//je veux envoyer à la fonction affichage_complet que le chapitre qu'elle doit afficher, donc je commence avec le chapitre 1.
-    if(affichage_complet(chapitre_actuel,&tableau_inventaire,id_prochain_chapitre)!=0){
-        perror("Erreur Affichage : voir Alexandre (c'est de sa faute)\n");
-        exit(1);
-    }
-    printf("%d",*id_prochain_chapitre);
+    int* id_prochain_chapitre;
+    // struct Chapter* chapitre_actuel = &BigTableau.chapter[0];//je veux envoyer à la fonction affichage_complet que le chapitre qu'elle doit afficher, donc je commence avec le chapitre 1.
+    // if(affichage_complet(chapitre_actuel,&tableau_inventaire,id_prochain_chapitre)!=0){
+    //     perror("Erreur Affichage : voir Alexandre (c'est de sa faute)\n");
+    //     exit(1);
+    // }
+    // printf("%d",*id_prochain_chapitre);
 
 
 //Libération mémoire :
